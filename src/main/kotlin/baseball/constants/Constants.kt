cc13b0aca594b15663530c30747c10085f5d082d
박지민 <pjm5489@naver.com>
2024-09-26 02:25:49 +0900

    feat: 상수추가

---
+++ src/main/kotlin/baseball/constants/Constants.kt
@@ -0,0 +1,18 @@
+package baseball.constants
+
+
+object Constants {
+    const val GAME_START_MSG = "숫자야구 게임을 시작합니다."
+    const val GAME_END_MSG = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+    const val INPUT_NUMBER_MSG = "숫자를 입력해주세요:"
+    const val STRIKE_MSG = "스트라이크"
+    const val BALL_MSG = "볼"
+    const val NOTHING_MSG = "낫싱"
+    const val ERROR_MSG = "1이나 2를 입력해주세요."
+    const val START_OR_END = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+    const val MIN_NUMBER = 1
+    const val MAX_NUMBER = 9
+    const val RESTART_NUMBER = 1
+    const val END_NUMBER = 2
+    const val NUMBER_LENGTH = 3
+}
